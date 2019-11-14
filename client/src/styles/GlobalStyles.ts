b25b7e7293c25147ed9d15ca0e5faa99051028af
Vianney <vianneyjchin@gmail.com>
2019-11-14 12:19:37 -0800

    reset font

--- client/src/styles/GlobalStyles.ts
+++ client/src/styles/GlobalStyles.ts
@@ -6,6 +6,7 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     box-sizing: border-box;
     font-size: 14px;
+    font-weight: normal;
     text-decoration: none;
     text-rendering: optimizeLegibility;
     -webkit-font-smoothing: auto;
